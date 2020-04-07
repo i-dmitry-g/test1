d450af8bb6b8b652cb495e481d49d7a48edc6b4c
i-dmitry-g <45047617+i-dmitry-g@users.noreply.github.com>
2020-04-07 13:39:40 +0400

    Update program.py

--- program.py
+++ program.py
@@ -1,11 +1,2 @@
-from flask import Flask
+print('test')
 
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    return "Привет от приложения Flask"
-
-
-if __name__ == '__main__':
-    app.run()
